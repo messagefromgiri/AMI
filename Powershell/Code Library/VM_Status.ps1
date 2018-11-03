6b4ee80a4ff0d253f384cc70e2ab5a5e48519100
Seshagiri Rao KB <messagefromgiri@live.com>
2018-11-03 11:05:50 +0530

    Add files via upload

---
+++ Powershell/Code Library/VM_Status.ps1
@@ -0,0 +1,19 @@
+﻿$null = Login-AzureRmAccount 
+
+$null = set-azurermcontext -subscription PS-EXT-QA-CMPL-USE
+
+$VMs = Get-AzureRmVM -ResourceGroupName "ASR-rg" 
+    
+    foreach($VM in $VMs)
+    {
+      $VMDetail = Get-AzureRmVM -ResourceGroupName "ASR-rg" -Name $VM.Name -Status
+      
+      foreach ($VMStatus in $VMDetail.Statuses)
+      { 
+          $VMStatusDetail = $VMStatus.DisplayStatus
+      }
+      Write-Output  ("VM Name: " + $VM.Name), "Status: $VMStatusDetail" `n
+    }
+
+
+
---
+++ Powershell/Code Library/VM_Status_Script.ps1
@@ -0,0 +1,57 @@
+﻿<#
+.SYNOPSIS
+    Get a list of Azure VMs and their Status within a given resource group.
+
+.DESCRIPTION
+    The Get-AzureRmVMStatus helps to you get a list of Azure VMs and their
+    status (PowerState) within a given resource group. You can supply a
+    VM name filter if you want to enclose only specific VMs in the result.
+
+.EXAMPLE
+    Get-AzureRmVMStatus -ResourceGroupName 'myrg01'
+
+.EXAMPLE
+    Get-AzureRmVMStatus -ResourceGroupName 'myrg01' -Name '*desktop*'
+
+.NOTES
+    Author : Sesh
+    Date   : 2018-10-25
+#>
+
+
+  [CmdletBinding()]
+  param (
+    #The name of a resouce group in your subscription
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ResourceGroupName,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $Subscription
+    ,
+    #VM name filter
+    [Parameter()]
+    [string]
+    $Name = '*'
+  )
+
+$null = Login-AzureRmAccount 
+
+$null = set-azurermcontext -subscription $Subscription
+
+  Get-AzureRmVM -ResourceGroupName $ResourceGroupName |
+    Get-AzureRmVM -Status |
+    Select-Object -Property Name, Statuses |
+    Where-Object {$_.Name -like $Name} |
+    ForEach-Object {
+      $VMName = $_.Name
+      $_.Statuses |
+        Where-Object {$_.Code -like 'PowerState/*'} |
+        ForEach-Object {
+          New-Object -TypeName psobject -Property @{
+            Name   = $VMName
+            Status = $_.DisplayStatus
+          }
+        }
+      }
